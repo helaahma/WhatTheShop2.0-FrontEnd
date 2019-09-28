f8c0359e14c1d307f57ec51b980aedcb231d3bc8
Saoud Alfares <sfares85@gmail.com>
2019-09-28 20:05:48 +0300

    scrolling on add watch working

--- components/Main/addWatch.js
+++ components/Main/addWatch.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { KeyboardAvoidingView, ScrollView } from "react-native";
 
 // NativeBase Components
 
@@ -7,8 +8,6 @@ import {
   Form,
   Item,
   Input,
-  ScrollView,
-  StyleSheet,
   Button,
   Text,
   Container,
@@ -40,149 +39,145 @@ class AddWatch extends Component {
   navigation = this.props.navigation;
   handlesubmit = () => {
     console.log("watch state", this.state);
-    watchStore.addWatch(this.state, navigation);
+    watchStore.addWatch(this.state);
   };
 
   render() {
     return (
       <Container>
-        <Content
-          contentContainerStyle={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <Form style={{ width: "80%" }}>
-            <Item>
-              <Input
-                placeholder="Brand"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={brand => this.setState({ brand })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="model name"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={model_name => this.setState({ model_name })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="case size"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={case_size => this.setState({ case_size })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="thickness"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={thickness => this.setState({ thickness })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="lug width"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={lug_width => this.setState({ lug_width })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="water resistance"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={water_resistance =>
-                  this.setState({ water_resistance })
-                }
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="manufacture year"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={manufacture_year =>
-                  this.setState({ manufacture_year })
-                }
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="price"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={price => this.setState({ price })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="box & papers"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={box_papers => this.setState({ box_papers })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="strap"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={strap => this.setState({ strap })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="functions"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={functions => this.setState({ functions })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="movement"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={movement => this.setState({ movement })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="payment"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={payment => this.setState({ payment })}
-              />
-            </Item>
-            <Item>
-              <Input
-                placeholder="delivery"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={delivery => this.setState({ delivery })}
-              />
-            </Item>
-            <Item last>
-              <Input
-                placeholder="description"
-                autoCapitalize="none"
-                autoCorrect={false}
-                onChangeText={description => this.setState({ description })}
-              />
-            </Item>
+        <Content>
+          <ScrollView>
+            <Form>
+              <Item>
+                <Input
+                  placeholder="Brand"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={brand => this.setState({ brand })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="model name"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={model_name => this.setState({ model_name })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="case size"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={case_size => this.setState({ case_size })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="thickness"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={thickness => this.setState({ thickness })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="lug width"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={lug_width => this.setState({ lug_width })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="water resistance"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={water_resistance =>
+                    this.setState({ water_resistance })
+                  }
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="manufacture year"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={manufacture_year =>
+                    this.setState({ manufacture_year })
+                  }
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="price"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={price => this.setState({ price })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="box & papers"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={box_papers => this.setState({ box_papers })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="strap"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={strap => this.setState({ strap })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="functions"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={functions => this.setState({ functions })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="movement"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={movement => this.setState({ movement })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="payment"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={payment => this.setState({ payment })}
+                />
+              </Item>
+              <Item>
+                <Input
+                  placeholder="delivery"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={delivery => this.setState({ delivery })}
+                />
+              </Item>
+              <Item last>
+                <Input
+                  placeholder="description"
+                  autoCapitalize="none"
+                  autoCorrect={false}
+                  onChangeText={description => this.setState({ description })}
+                />
+              </Item>
 
-            <Button full onPress={() => this.handlesubmit()}>
-              <Text>Add Watch</Text>
-            </Button>
-          </Form>
+              <Button full onPress={() => this.handlesubmit()}>
+                <Text>Add Watch</Text>
+              </Button>
+            </Form>
+          </ScrollView>
         </Content>
       </Container>
     );
